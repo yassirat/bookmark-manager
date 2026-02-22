aa815e6b3039d7dc7665cf655c6020cd84ecb944
Yassir <atertay@gmail.com>
2026-02-22 07:56:51 +0000

    Adding the mode icon

--- src/components/ui/svg-icons.tsx
+++ src/components/ui/svg-icons.tsx
@@ -153,7 +153,7 @@ export const DeleteIcon = () => {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      strokeWidth={2}
+      strokeWidth={2.5}
       stroke="currentColor"
       className="size-6 p-1 bg-red-700 text-white rounded-md transition-colors hover:bg-red-600">
       <path
@@ -198,6 +198,34 @@ export const GlobeIcon = () => {
         strokeLinejoin="round"
         d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
       />
+    </svg>
+  );
+};
+
+export const ThemeIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="size-6"
+      data-darkreader-inline-stroke="">
+      <path
+        stroke="none"
+        d="M0 0h24v24H0z"
+        fill="none"
+        data-darkreader-inline-stroke=""></path>
+      <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
+      <path d="M12 3l0 18"></path>
+      <path d="M12 9l4.65 -4.65"></path>
+      <path d="M12 14.3l7.37 -7.37"></path>
+      <path d="M12 19.6l8.85 -8.85"></path>
     </svg>
   );
 };
