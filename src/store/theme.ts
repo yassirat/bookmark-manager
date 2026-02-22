28e83883c1adcf02e3a011733b208e415cf86ccd
Yassir <atertay@gmail.com>
2026-02-22 07:55:47 +0000

    Adding a new store to handle theme changing

---
+++ src/store/theme.ts
@@ -0,0 +1,23 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+type Theme = 'light' | 'dark';
+
+interface ThemeProps {
+  theme: Theme;
+  toggleTheme: () => void;
+}
+
+export const useThemeStore = create<ThemeProps>()(
+  persist(
+    (set) => ({
+      theme: 'dark',
+      toggleTheme: () =>
+        set((state) => {
+          const newTheme = state.theme === 'light' ? 'dark' : 'light';
+          return { theme: newTheme };
+        }),
+    }),
+    { name: 'theme-storage' },
+  ),
+);
