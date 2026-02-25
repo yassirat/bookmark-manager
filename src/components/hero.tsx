b65e4ee662d621d89c2f922e5c73fd9379654ea9
Yassir <atertay@gmail.com>
2026-02-25 06:45:43 +0000

    refactor: Add button to hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,40 +2,51 @@ import { useBook, useDeleteBook } from '@/store/bookmark';
 import { useShowStore } from '@/store/show-form';
 import Image from 'next/image';
 import BookForm from './book-form';
-import { DeleteIcon } from './ui/svg-icons';
+import { CalendarIcon, DeleteIcon, PlusIcon } from './ui/svg-icons';
 
 export default function Hero() {
   const books = useBook();
   const deleteBook = useDeleteBook();
-  const { show } = useShowStore();
+  const show = useShowStore((state) => state.show);
+  const showForm = useShowStore((state) => state.showForm);
 
-  const time = new Date();
+  // const time = new Date();
 
   return (
     <article className="bg-blue-50 py-6 px-8 dark:bg-neutral-900 dark:text-white">
       {show && <BookForm />}
       {books.length == 0 ? (
-        <p className="font-medium text-xl h-full flex items-center justify-center text-center">
-          You don’t have bookmarks yet. Add your first one.
-        </p>
+        <div className="h-full grid place-items-center place-content-center gap-4">
+          <p className="font-medium text-xl text-center">
+            You don’t have bookmarks yet. Add your first one.
+          </p>
+          <button
+            type="button"
+            className="bg-[#273f4f] text-white  flex items-center gap-2 py-2 px-4 rounded-md transition-colors hover:bg-[rgba(39,63,79,.90)]"
+            onClick={showForm}
+            aria-label="Open the bookmark form">
+            <PlusIcon />
+            <span className="text-xs font-medium lg:text-sm">Add Bookmark</span>
+          </button>
+        </div>
       ) : (
         <section className="max-w-6xl mx-auto">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-5 gap-4 place-items-center">
             {/* First grid */}
             {books.map((book) => (
               <div
-                className="w-64 h-56 rounded-lg bg-white/80 dark:bg-neutral-900/80 dark:text-white shadow flex flex-col justify-between gap-4"
+                className="w-68 h-56 rounded-lg bg-white/80 dark:bg-neutral-900/80 dark:text-white shadow flex flex-col justify-between gap-4"
                 style={{
                   boxShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px',
                 }}
                 key={book.id}>
                 <div className="flex items-center justify-between border-b-2 border-b-gray-300 py-4 px-4 dark:border-b-gray-700">
                   <div className="flex items-center gap-3">
-                    <div className="shadow bg-white dark:bg-black rounded-md p-1">
+                    <div className="shadow">
                       <Image
                         src={book.faviconUrl}
                         alt={`${book.name}'s favicon`}
-                        className="text-xs overflow-hidden"
+                        className="text-xs overflow-hidden rounded-sm"
                         width={30}
                         height={30}
                       />
@@ -49,7 +60,7 @@ export default function Hero() {
                       <a
                         href={book.link}
                         target="_blank"
-                        className="text-sm text-gray-500 dark:text-gray-300 transition-colors hover:text-gray-900">
+                        className="text-sm text-gray-500 dark:text-gray-300 transition-colors hover:text-gray-900 dark:hover:text-white">
                         {book.link.slice(8, length - 1)}
                       </a>
                     </div>
@@ -71,10 +82,14 @@ export default function Hero() {
                 </div>
 
                 {/* Third Grid */}
-                <div className="px-4 py-2 border-t-2 border-t-gray-300 dark:border-t-gray-700">
-                  <p className="font-medium text-sm text-gray-500 dark:text-gray-400">
-                    {time.getDate()}{' '}
-                    {time.toLocaleString('en-US', { month: 'short' })}
+                <div className="px-4 py-2 flex items-center gap-2 border-t-2 border-t-gray-300 dark:border-t-gray-700">
+                  <CalendarIcon />
+                  <p className="font-medium text-xs text-gray-500 dark:text-gray-400">
+                    {book.createdAt.toLocaleDateString('en-US', {
+                      year: 'numeric',
+                      month: 'short',
+                      day: 'numeric',
+                    })}
                   </p>
                 </div>
               </div>
