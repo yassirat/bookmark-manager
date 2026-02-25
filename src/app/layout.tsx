fa6dc2ad3983e56dfa8c179d7bf78cee15a6d27c
Yassir <atertay@gmail.com>
2026-02-25 06:44:09 +0000

    style: Change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import ThemeProvider from '@/components/ui/theme-provider';
 import type { Metadata } from 'next';
-import { Roboto } from 'next/font/google';
+import { Arimo } from 'next/font/google';
 import './globals.css';
 
-const mainFont = Roboto({
+const mainFont = Arimo({
   variable: '--font-main',
   subsets: ['latin'],
   display: 'swap',
